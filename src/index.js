d6fb227ca86c9f4af5afcc5fa52d4281f91cdc7a
eps1lon <silbermann.sebastian@gmail.com>
2021-09-10 09:05:32 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function main(userFlag) {
+  let flag = true;
+  // Just need some syntax that needs transpilation
+  flag &&= userFlag;
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  target: "browserslist",
+};
